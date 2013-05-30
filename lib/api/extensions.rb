f6a7997222e4912b94fe10625967ee63a3e4af00
Nahuel Cuesta Luengo <nahuelcuestaluengo@gmail.com>
2013-05-30 11:27:49 -0300

    Added convenience module for including `All` the extensions.

--- lib/api/extensions.rb
+++ lib/api/extensions.rb
@@ -4,5 +4,14 @@ require 'api/extensions/version'
 
 module Api
   module Extensions
+    module All
+      # Convenience module for including all extensions at once
+      def self.included(base)
+        base.class_eval %Q{
+          include Api::Extensions::Expand
+          include Api::Extensions::Fields
+        }
+      end
+    end
   end
 end
--- lib/api/extensions/version.rb
+++ lib/api/extensions/version.rb
@@ -1,5 +1,5 @@
 module Api
   module Extensions
-    VERSION = '0.0.2'
+    VERSION = '0.0.3'
   end
 end
